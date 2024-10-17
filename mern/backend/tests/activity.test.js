0f12a2f331604df05c8af2dd8e145a8558e6ca1a
UdayBadola <109196152+UdayBadola@users.noreply.github.com>
2024-10-17 10:14:00 +0530

    test case

--- mern/backend/tests/activity.test.js
+++ mern/backend/tests/activity.test.js
@@ -1,18 +1,17 @@
-test('fake API test', async () => {
-    // Simulate making an API request (but don't actually do it)
+test('API test', async () => {
+    
     const API = new Promise((resolve) => {
       setTimeout(() => {
         resolve({
           status: 200,
           data: { message: 'Success' }
         });
-      }, 1000); // Simulating 1-second network delay
+      }, 1000); 
     });
   
-    // "Await" the fake API response
+   
     const response = await API;
   
-    // Simulate assertions for the fake API response
     expect(response.status).toBe(200);
     expect(response.data.message).toBe('Success');
   });
